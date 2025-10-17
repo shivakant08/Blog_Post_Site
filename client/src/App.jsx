4d321c4e18351b6c164aacb8b85f72908f018e90
shivakant08 <shivakantgiri08@gmail.com>
2025-10-17 19:56:07 +0530

    Auth Context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,19 +2,30 @@ import { useState } from 'react'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
 import Hero from './Components/Hero'
+import AuthModal from './Modal/AuthModal'
 
 
 function App() {
-
+  const [isAuthOpen, setIsAuthOpen] = useState(false)
+  const [authType, setAuthType] = useState("signin")
 
   return (
     <>
 
-      <Header />
+      <Header
+        onOpenAuthModal={(type) => {
+          setAuthType(type)
+          setIsAuthOpen(true)
+        }}
+      />
       <Hero />
       <Footer />
 
-
+      <AuthModal
+        isOpen={isAuthOpen}
+        type={authType}
+        onClose={() => setIsAuthOpen(false)}          // closes modal
+        onSwitch={(newType) => setAuthType(newType)} />
 
     </>
   )
