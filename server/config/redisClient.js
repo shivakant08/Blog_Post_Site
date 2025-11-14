414747dffb4f17fc804c154cf84e1b95ac0687bc
shivakant08 <shivakantgiri08@gmail.com>
2025-11-14 19:56:35 +0530

    Updated the redisClient file

--- server/config/redisClient.js
+++ server/config/redisClient.js
@@ -1,14 +1,16 @@
 import Redis from "ioredis"
-const redis = new Redis({
-    host:"127.0.0.1",
-    port: 6379
-})
+// const redis = new Redis({
+//     host:"127.0.0.1",
+//     port: 6379
+// })
 
-redis.on("connect", ()=>{
+const redis = new Redis(process.env.REDIS_URL || "redis://127.0.0.1:6379")
+
+redis.on("connect", () => {
     console.log("✅Connected to Redis")
 })
 
-redis.on("error", (err)=>{
+redis.on("error", (err) => {
     console.error("❌Redis error", err)
 })
 // await redis.connect()
