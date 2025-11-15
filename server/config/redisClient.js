81d43ef6e107329907e7d25ea5061ccf9b8fbe1e
shivakant08 <shivakantgiri08@gmail.com>
2025-11-15 10:50:57 +0530

    Update the redisClient file

--- server/config/redisClient.js
+++ server/config/redisClient.js
@@ -1,18 +1,37 @@
-import Redis from "ioredis"
-// const redis = new Redis({
-//     host:"127.0.0.1",
-//     port: 6379
+// import Redis from "ioredis"
+// // const redis = new Redis({
+// //     host:"127.0.0.1",
+// //     port: 6379
+// // })
+
+// const redis = new Redis(process.env.REDIS_URL || "redis://127.0.0.1:6379")
+
+// redis.on("connect", () => {
+//     console.log("✅Connected to Redis")
 // })
 
-const redis = new Redis(process.env.REDIS_URL || "redis://127.0.0.1:6379")
+// redis.on("error", (err) => {
+//     console.error("❌Redis error", err)
+// })
+// // await redis.connect()
 
-redis.on("connect", () => {
-    console.log("✅Connected to Redis")
-})
+// export default redis
 
-redis.on("error", (err) => {
-    console.error("❌Redis error", err)
-})
-// await redis.connect()
+import Redis from "ioredis";
 
-export default redis
+let redis = null;
+
+if (process.env.REDIS_URL) {
+  redis = new Redis(process.env.REDIS_URL, {
+    tls: {
+      rejectUnauthorized: false,
+    }
+  });
+
+  redis.on("connect", () => console.log("✅ Connected to Redis"));
+  redis.on("error", (err) => console.error("❌ Redis error", err));
+} else {
+  console.log("⚠️ No Redis URL provided. Redis is disabled on this server.");
+}
+
+export default redis;
