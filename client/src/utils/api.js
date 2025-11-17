9271919f4564672b28173f823773cc229b399e7d
shivakant08 <shivakantgiri08@gmail.com>
2025-11-17 14:43:13 +0530

    Modified all the pages

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,9 +1,14 @@
 // src/utils/api.js
 import axios from "axios";
 
+// Export BASE_URL so it can be imported everywhere
+export const BASE_URL =
+  import.meta.env.VITE_BASE_URL || "http://localhost:5000";
+
+// Axios instance
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL || "http://localhost:5000",
-  withCredentials: true, // if you ever use cookies
+  baseURL: BASE_URL,
+  withCredentials: true,
 });
 
 export default api;
